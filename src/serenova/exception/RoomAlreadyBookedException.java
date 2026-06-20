004c9988c5b9dd714467d6021423325de249b84b
Nadheeshan <rkshan1569@gmail.com>
2026-06-20 15:32:02 +0530

    Add RoomAlreadyBookedException

---
+++ src/serenova/exception/RoomAlreadyBookedException.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package serenova.exception;
+
+/**
+ *
+ * @author HP
+ */
+public class RoomAlreadyBookedException extends Exception {
+    
+    public RoomAlreadyBookedException (String message){
+        super(message);
+    }
+    
+}
